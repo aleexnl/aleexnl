032037337b69ee352d2d9cde65bf2198412304db
“aleexnl” <alex.nieto0027@gmail.com>
2021-08-18 15:54:58 +0200

    Added MaterialUI v5, created SSR handler & removed unused/not needed files.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "lib/theme";
+import createEmotionCache from "lib/createEmotionCache";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}) {
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>Home</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
