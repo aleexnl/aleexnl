81a9c3de5bbd74625535cbb786b1e0608843928a
aleexnl <alex.nieto0027@gmail.com>
2025-03-09 15:10:32 +0100

    refactor: simplify ESLint configuration by removing unused imports and enhancing Next.js plugin integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,8 @@
 // @ts-check
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
-import react from "eslint-plugin-react";
 import nextPlugin from "@next/eslint-plugin-next";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -34,6 +19,12 @@ export default tseslint.config(
       ],
       "react/no-unescaped-entities": "off",
     },
+  },
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: nextPlugin.configs["core-web-vitals"].rules,
   },
   eslintConfigPrettier
 );
