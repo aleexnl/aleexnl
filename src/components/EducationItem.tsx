0da5ae77a7e5a5a96fc2a09806c5c6b13e53d902
aleexnl <alex.nieto0027@gmail.com>
2025-03-09 22:32:38 +0100

    feat: add gap between title and DateChip in EducationItem and ExperienceItem for improved layout

--- src/components/EducationItem.tsx
+++ src/components/EducationItem.tsx
@@ -15,7 +15,7 @@ export function EducationItem({
   return (
     <div className="relative pl-8 border-l-2 border-blue-100 dark:border-blue-900">
       <div className="absolute w-4 h-4 bg-blue-500 rounded-full -left-[9px] top-1"></div>
-      <div className="flex justify-between items-start">
+      <div className="flex justify-between items-start gap-8">
         <h3 className="text-xl font-semibold">{title}</h3>
         <DateChip date={period} />
       </div>
